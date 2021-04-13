8461161243c3522cfc02bfb761736fb907ea2831
Sophia Castillo <Sphcastillo@gmail.com>
2021-04-13 12:31:46 -0700

    about to create task.js

--- task-draganddrop/src/index.js
+++ task-draganddrop/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import initialData from './initial-data';
+import Column from "./column";
+import '@atlaskit/css-reset';
 
 class App extends React.Component {
     state = initialData;
@@ -10,8 +12,8 @@ class App extends React.Component {
             const column = this.state.columns[columnId];
             const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
 
-            return column.title;
-        })
+            return <Column key={column.id} column={column} tasks={tasks} />;
+        });
     }
 }
 ReactDOM.render(<App />, document.getElementById('root'));
