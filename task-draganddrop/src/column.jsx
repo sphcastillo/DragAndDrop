7758d41b113a7955e9d1f2fe776a7e9eb09e39ae
Sophia Castillo <Sphcastillo@gmail.com>
2021-04-15 12:00:21 -0700

    not moving

--- task-draganddrop/src/column.jsx
+++ task-draganddrop/src/column.jsx
@@ -15,27 +15,22 @@ const TaskList = styled.div`
   padding: 8px;
 `;
 
-export default class Column extends React.Component{
-    render(){
-        return (
-            <Container>
-                <Title>{this.props.column.title}</Title>
-                <Droppable
-                    droppableId= {this.props.column.id}
-                >
-                    {provided => (
-                    <TaskList
-                        innerRef={provided.innerRef}
-                        {...provided.droppableProps}
-                    >
-                        {this.props.tasks.map((task, index) => (
-                            <Task key={task.id} task={task} index={index}/>
-                        ))}
-                        {provided.placeholder}
-                    </TaskList>
-                    )}
-                </Droppable>
-            </Container>
-        )
-    }
+export default class Column extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Title>{this.props.column.title}</Title>
+        <Droppable droppableId={this.props.column.id}>
+          {provided => (
+            <TaskList innerRef={provided.innerRef} {...provided.droppableProps}>
+              {this.props.tasks.map((task, index) => (
+                <Task key={task.id} task={task} index={index} />
+              ))}
+              {provided.placeholder}
+            </TaskList>
+          )}
+        </Droppable>
+      </Container>
+    );
+  }
 }
--- task-draganddrop/src/index.js
+++ task-draganddrop/src/index.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import initialData from './initial-data';
-import Column from "./column";
 import '@atlaskit/css-reset';
 import { DragDropContext } from 'react-beautiful-dnd';
+import initialData from './initial-data';
+import Column from './column';
 
 class App extends React.Component {
-    state = initialData;
+  state = initialData;
 
-    onDragEnd = result => {
-        // TODO: reorder our column
-    }
+  onDragEnd = result => {
+    // TODO: reorder our column
+  };
 
-    render(){
-        return (
-            <DragDropContext
-                onDragEnd = {this.onDragEnd}
-            >
-                {this.state.columnOrder.map(columnId => {
-                    const column = this.state.columns[columnId];
-                    const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+  render() {
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        {this.state.columnOrder.map(columnId => {
+          const column = this.state.columns[columnId];
+          const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
 
-                return <Column key={column.id} column={column} tasks={tasks} />;
-                })};
-            </DragDropContext>
-        );
-    }
+          return <Column key={column.id} column={column} tasks={tasks} />;
+        })}
+      </DragDropContext>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
