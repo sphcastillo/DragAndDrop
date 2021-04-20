51200ecf0dcd2650bba1f1e593b2e3792aee368b
Sophia Castillo <Sphcastillo@gmail.com>
2021-04-20 11:46:10 -0700

    customizing the appearing using onDragState and onDragEnd

--- task-draganddrop/src/App.js
+++ task-draganddrop/src/App.js
@@ -9,48 +9,66 @@ import Column from './components/Column';
 class App extends React.Component {
     state = initialData;
 
-    onDragEnd = result => {
-      const { destination, source, draggableId } = result;
-
-      if(!destination){
-          return;
-
-      }
-
-      if(
-          destination.droppableId === source.droppableId &&
-          destination.index === source.index
-      ){
-          return;
-      }
-
-      const column = this.state.columns[source.droppableId];
-      const newTaskIds = Array.from(column.taskIds);
-      newTaskIds.splice(source.index, 1);
-      newTaskIds.splice(destination.index, 0, draggableId);
-
-      const newColumn = {
-          ...column, 
-          taskIds: newTaskIds,
-      };
-
-      const newState = {
-          ...this.state,
-          columns: {
-              ...this.state.columns,
-              [newColumn.id]: newColumn,
-          },
-      }
-
-      this.setState(newState);
+    onDragStart = () => {
+        document.body.style.color = 'orange';
+        document.body.style.transition = 'background-color 0.2s ease';
     }
 
-    
+    onDragUpdate = update => {
+        const { destination } = update;
+        const opacity = destination
+        ? destination.index / Object.keys(this.state.tasks).length
+        : 0;
+        document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`;
+    }
 
-  
+    onDragEnd = result => {
+
+        document.body.style.color = 'inherit';
+        document.body.style.backgroundColor = 'inherit';
+        
+        const { destination, source, draggableId } = result;
+
+        if(!destination){
+            return;
+
+        }
+
+        if(
+            destination.droppableId === source.droppableId &&
+            destination.index === source.index
+        ){
+            return;
+        }
+
+        const column = this.state.columns[source.droppableId];
+        const newTaskIds = Array.from(column.taskIds);
+        newTaskIds.splice(source.index, 1);
+        newTaskIds.splice(destination.index, 0, draggableId);
+
+        const newColumn = {
+            ...column, 
+            taskIds: newTaskIds,
+        };
+
+        const newState = {
+            ...this.state,
+            columns: {
+                ...this.state.columns,
+                [newColumn.id]: newColumn,
+            },
+        }
+
+        this.setState(newState);
+    }
+
     render(){
         return(
-            <DragDropContext onDragEnd={this.onDragEnd}>
+            <DragDropContext 
+            onDragStart={this.onDragStart}
+            onDragUpdate={this.onDragUpdate}
+            onDragEnd={this.onDragEnd}
+            >
             {this.state.columnOrder.map(columnId => {
               const column = this.state.columns[columnId];
               const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
