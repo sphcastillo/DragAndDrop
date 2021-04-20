a8dc371cbecf7d01940f34c4ab61c65b30c8e3fb
Sophia Castillo <Sphcastillo@gmail.com>
2021-04-20 11:31:16 -0700

    customized the appearance of an App during a Drag using snapshot values

--- task-draganddrop/src/components/Task.js
+++ task-draganddrop/src/components/Task.js
@@ -7,7 +7,7 @@ const Container = styled.div`
   border-radius: 2px;
   padding: 8px;
   margin-bottom: 8px;
-  background-color: white;
+  background-color:${prop => (prop.isDragging ? 'lightgreen': 'white')}
 `;
 
 export default class Task extends React.Component {
@@ -21,6 +21,7 @@ export default class Task extends React.Component {
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             ref={provided.innerRef}
+            isDragging={snapshot.isDragging}
           >
             {this.props.task.content}
           </Container>
